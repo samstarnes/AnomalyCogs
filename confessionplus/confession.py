f94050730eb3d8d7d6da04654bbb4f6dd2814192
0x0 <an0n1sl3g10n@gmail.com>
2022-04-19 23:18:17 -0400

    Update confession.py

--- confessionplus/confession.py
+++ confessionplus/confession.py
@@ -107,8 +107,8 @@ class Confession(BaseCog):
 
         try:
             await ctx.bot.send_filtered(destination=confession_room, content=confession)
-            await clog.send(ctx.author)
             clog = client.get_channel(966170510394286090)
+            await clog.send(ctx.author)
         except discord.errors.Forbidden:
             return await ctx.author.send("I don't have permission to send messages to this room or something went wrong.")
             
