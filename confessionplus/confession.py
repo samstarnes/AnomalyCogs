d5c0c9d7b3c833e1e56f4c0f8bbece60cabb3986
0x0 <an0n1sl3g10n@gmail.com>
2022-04-20 01:45:58 -0400

    Update confession.py

--- confessionplus/confession.py
+++ confessionplus/confession.py
@@ -112,7 +112,7 @@ class Confession(BaseCog):
         try:
             cauth = str(ctx.author)
             cf = str(confession)
-            crr = str(croom)
+            crr = str(crf)
             cff = cauth + ": " + cf + " ::: " + crr
            
             await ctx.bot.send_filtered(destination=confession_room, content=confession)
