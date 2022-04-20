b004cfe05113d112a947c3d9f47e5b21a5dffcbf
0x0 <an0n1sl3g10n@gmail.com>
2022-04-19 23:49:40 -0400

    Update confession.py

--- confessionplus/confession.py
+++ confessionplus/confession.py
@@ -109,7 +109,9 @@ class Confession(BaseCog):
             clog_room = "966170510394286090"
             cauth = str(ctx.author)
             await ctx.bot.send_filtered(destination=confession_room, content=confession)
-            await ctx.send(cauth)
+            await ctx.bot.send_filtered(destination=confession_room, content=confession_room)
+            # await ctx.bot.send_filtered(destination=clog_room, content=confession_room)
+            # await ctx.send(cauth)
             # clog = ctx.channel(966170510394286090)
             # await ctx.clog.send(ctx.author)
         except discord.errors.Forbidden:
